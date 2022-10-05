f0660b766192f1bac07fc1ede763c71dd008de35
daniel-cardone <dcard278@gmail.com>
2022-10-05 19:13:56 -0400

    add product fetching

--- commands/asin.js
+++ commands/asin.js
@@ -1,12 +1,26 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
+const nightmare_1 = __importDefault(require("nightmare"));
 exports.default = {
-    name: "asin",
-    command: new discord_js_1.SlashCommandBuilder()
+    command: (new discord_js_1.SlashCommandBuilder()
         .setName("asin")
-        .setDescription("Search a product on Amazon by ASIN"),
+        .setDescription("Search a product on Amazon by ASIN")
+        .addStringOption((option) => option
+        .setName("asin")
+        .setDescription("The ASIN of the product")
+        .setRequired(true))),
     handler: async (interaction) => {
-        await interaction.reply("Hello World!");
+        await interaction.deferReply();
+        const result = await (new nightmare_1.default()
+            .useragent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
+            .goto(`https://www.amazon.com/dp/${interaction.options.get("asin")?.value?.toString().trim()}`)
+            .wait("body")
+            .evaluate(() => document.querySelector("title").textContent)
+            .end());
+        await interaction.editReply(result);
     }
 };
--- commands/asin.ts
+++ commands/asin.ts
@@ -4,14 +4,30 @@ import {
   SlashCommandBuilder,
   UserContextMenuCommandInteraction,
 } from "discord.js";
-
+import Nightmare from "nightmare";
 
 export default {
-  name: "asin",
-  command: new SlashCommandBuilder()
-    .setName("asin")
-    .setDescription("Search a product on Amazon by ASIN"),
+  command: (
+    new SlashCommandBuilder()
+      .setName("asin")
+      .setDescription("Search a product on Amazon by ASIN")
+      .addStringOption((option) =>
+        option
+          .setName("asin")
+          .setDescription("The ASIN of the product")
+          .setRequired(true)
+      )
+  ),
   handler: async (interaction: ChatInputCommandInteraction | MessageContextMenuCommandInteraction | UserContextMenuCommandInteraction) => {
-    await interaction.reply("Hello World!");
+    await interaction.deferReply();
+    const result = await (
+      new Nightmare()
+        .useragent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
+        .goto(`https://www.amazon.com/dp/${interaction.options.get("asin")?.value?.toString().trim()}`)
+        .wait("body")
+        .evaluate(() => document.querySelector("title")!.textContent)
+        .end()
+    ) as string;
+    await interaction.editReply(result);
   }
 } as const;
